ea1fe9cee34794f7251608f3bb85d4e5f7775e95
suzukiryoji <suzukiryoji@localhost>
2021-04-26 20:56:18 +0900

    Write codes

--- src/code14/Main.java
+++ src/code14/Main.java
@@ -3,8 +3,9 @@ package code14;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		Empty e = new Empty();
+		String s = e.toString();
+		System.out.println(s);
 	}
 
 }
