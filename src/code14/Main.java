22d87b5d620e0bfa13094c0a864333a15dd0a698
suzukiryoji <suzukiryoji@localhost>
2021-04-27 18:35:05 +0900

    Write codes

--- src/code14/Main.java
+++ src/code14/Main.java
@@ -9,6 +9,8 @@ public class Main {
 		Hero h2 = new Hero();
 		h2.name = "ミナト";
 		h2.hp = 100;
+		System.out.println(h1.hp);
+		System.out.println(Hero.money);
 		if (h1.equals(h2) == true) {
 			System.out.println("同じ内容です");
 		} else {
