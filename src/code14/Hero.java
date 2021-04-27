a47c78a67f01619d439a03d6914b089fb2d76393
suzukiryoji <suzukiryoji@localhost>
2021-04-28 03:55:59 +0900

    Write codes

--- src/code14/Hero.java
+++ src/code14/Hero.java
@@ -5,6 +5,11 @@ public class Hero {
 	int hp;
 	static int money;
 
+	public static void setRandomMoney() {
+		Hero.money = (int)(Math.random() * 1000);
+		System.out.println(this.name + "たちの所持金を初期化しました");
+	}
+
 	public String toString() {
 		return "名前：" + this.name + "/HP：" + this.hp;
 	}
